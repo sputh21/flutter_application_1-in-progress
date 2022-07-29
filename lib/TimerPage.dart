2505d081e090efe63ba35d8918c67eb93074de2c
Sahil Puthawala <84691537+sputh21@users.noreply.github.com>
2022-07-29 00:30:56 -0400

    initial stopwatch build

--- lib/TimerPage.dart
+++ lib/TimerPage.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:stop_watch_timer/stop_watch_timer.dart';
 import 'package:flutter_application_1/navBar.dart';
+import 'dart:async';
 
 class CountDown extends StatefulWidget {
   const CountDown({Key? key}) : super(key: key);
@@ -10,29 +12,78 @@ class CountDown extends StatefulWidget {
 }
 
 class _CountDownState extends State<CountDown> {
-  final StopWatchTimer _stopWatch = StopWatchTimer();
-  final _isHour = true;
+  late Stopwatch _stopwatch;
+  late Timer t;
 
   @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-    _stopWatch.dispose();
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _stopwatch = Stopwatch();
+    t = Timer.periodic(Duration(milliseconds: 30), (timer) {
+      setState(() {});
+    });
+  }
+
+  String formatText() {
+    var milli = _stopwatch.elapsedMilliseconds;
+    String milliseconds = (milli % 1000).toString().padLeft(3, "0");
+    String seconds = ((milli ~/ 1000) % 60).toString().padLeft(2, "0");
+    String minutes = ((milli ~/ 1000) ~/ 60).toString().padLeft(3, "0");
+    return "$minutes:$seconds:$milliseconds";
+  }
+
+  void startStop() {
+    if (_stopwatch.isRunning) {
+      _stopwatch.stop();
+    } else {
+      _stopwatch.start();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey[700],
-      drawer: NavBar(),
-      appBar: AppBar(
-        title: const Text("LeetCodeCharts Home"),
-        backgroundColor: Colors.black,
-        foregroundColor: Color.fromARGB(227, 190, 74, 27),
-        centerTitle: true,
-        elevation: 10,
-        shadowColor: Colors.red,
-      ),
-    );
+        backgroundColor: Colors.blueGrey[700],
+        drawer: NavBar(),
+        appBar: AppBar(
+          title: const Text("LeetCodeCharts Home"),
+          backgroundColor: Colors.black,
+          foregroundColor: Color.fromARGB(227, 190, 74, 27),
+          centerTitle: true,
+          elevation: 10,
+          shadowColor: Colors.red,
+        ),
+        body: SafeArea(
+          child: Center(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CupertinoButton(
+                onPressed: () {
+                  startStop();
+                },
+                child: Container(
+                  height: 300,
+                  padding: EdgeInsets.all(0),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 5,
+                      )),
+                  child: Text(formatText(), style: TextStyle(fontSize: 40)),
+                ),
+              ),
+              CupertinoButton(
+                  child: Text("Reset",
+                      style: TextStyle(color: Colors.red, fontSize: 20)),
+                  onPressed: () {
+                    _stopwatch.reset();
+                  })
+            ],
+          )),
+        ));
   }
 }
