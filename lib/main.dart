eac8ed60d408c0f2ad0130af2085bf71a2e64737
Sahil Puthawala <84691537+sputh21@users.noreply.github.com>
2022-08-01 14:57:09 -0400

    scroll bar of companies

--- lib/main.dart
+++ lib/main.dart
@@ -13,16 +13,94 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey[700],
-      drawer: NavBar(),
-      appBar: AppBar(
-        title: const Text("LeetCodeCharts Home"),
-        backgroundColor: Colors.black,
-        foregroundColor: Color.fromARGB(227, 190, 74, 27),
-        centerTitle: true,
-        elevation: 10,
-        shadowColor: Colors.red,
-      ),
-    );
+        backgroundColor: Colors.blueGrey[700],
+        drawer: NavBar(),
+        appBar: AppBar(
+          title: const Text("LeetCodeCharts Home"),
+          backgroundColor: Colors.black,
+          foregroundColor: Color.fromARGB(227, 190, 74, 27),
+          centerTitle: true,
+          elevation: 10,
+          shadowColor: Colors.red,
+        ),
+        body: Container(
+            margin: EdgeInsets.symmetric(vertical: 20),
+            height: 150,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                SizedBox(width: 10),
+                Container(
+                  width: 160.0,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.red,
+                      // ignore: prefer_const_literals_to_create_immutables
+                      boxShadow: [
+                        BoxShadow(
+                            color: Color.fromARGB(129, 0, 0, 0),
+                            spreadRadius: 3)
+                      ]),
+                ),
+                SizedBox(width: 10),
+                Container(
+                  width: 160.0,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.blue,
+                      // ignore: prefer_const_literals_to_create_immutables
+                      boxShadow: [
+                        BoxShadow(
+                            color: Color.fromARGB(129, 0, 0, 0),
+                            spreadRadius: 3)
+                      ]),
+                ),
+                SizedBox(width: 10),
+                Container(
+                  width: 160.0,
+                  //color: Colors.green,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.green,
+                      // ignore: prefer_const_literals_to_create_immutables
+                      boxShadow: [
+                        BoxShadow(
+                            color: Color.fromARGB(129, 0, 0, 0),
+                            spreadRadius: 3)
+                      ]),
+                ),
+                SizedBox(width: 10),
+                Container(
+                  width: 160.0,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.yellow,
+                      // ignore: prefer_const_literals_to_create_immutables
+                      boxShadow: [
+                        BoxShadow(
+                            color: Color.fromARGB(129, 0, 0, 0),
+                            spreadRadius: 3)
+                      ]),
+                ),
+                SizedBox(width: 10),
+                Container(
+                  width: 160.0,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.orange,
+                      // ignore: prefer_const_literals_to_create_immutables
+                      boxShadow: [
+                        BoxShadow(
+                            color: Color.fromARGB(129, 0, 0, 0),
+                            spreadRadius: 3)
+                      ]),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(
+                    onPressed: () {},
+                    child: Text("hello"),
+                    style: ElevatedButton.styleFrom()),
+              ],
+            )));
   }
 }
