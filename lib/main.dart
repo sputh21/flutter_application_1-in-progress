63f14ab7588fb60b310f69d8e5256a3359063db1
Sahil Puthawala <84691537+sputh21@users.noreply.github.com>
2022-09-04 21:13:08 -0400

    google and netflix dialog boxes filled out

--- lib/main.dart
+++ lib/main.dart
@@ -25,186 +25,187 @@ class MyApp extends StatelessWidget {
           shadowColor: Colors.red,
         ),
         body: Container(
-            margin: EdgeInsets.symmetric(vertical: 20),
-            height: 150,
-            child: ListView(
-              scrollDirection: Axis.horizontal,
-              children: [
-                SizedBox(width: 10),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      shadowColor: Colors.white60,
-                      primary: Color.fromARGB(0, 255, 255, 255),
-                      padding: EdgeInsets.all(10),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20))),
-                  child: Container(
-                    height: 130,
-                    width: 150,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Color.fromARGB(255, 0, 0, 0),
-                      image: DecorationImage(
-                          colorFilter: ColorFilter.mode(
-                              Color.fromARGB(136, 0, 0, 0), BlendMode.darken),
-                          image: AssetImage('assets/Google.jpg'),
-                          fit: BoxFit.cover),
-                    ),
-                    child: Text("Google",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(height: 3.4, fontSize: 30)),
+          margin: EdgeInsets.symmetric(vertical: 20),
+          height: 150,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: [
+              SizedBox(width: 10),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    shadowColor: Colors.white60,
+                    primary: Color.fromARGB(0, 255, 255, 255),
+                    padding: EdgeInsets.all(10),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20))),
+                child: Container(
+                  height: 130,
+                  width: 150,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    image: DecorationImage(
+                        colorFilter: ColorFilter.mode(
+                            Color.fromARGB(136, 0, 0, 0), BlendMode.darken),
+                        image: AssetImage('assets/Google.jpg'),
+                        fit: BoxFit.cover),
                   ),
-                  // ),
-                  onPressed: () {
-                    showGoogleDialog(context);
-                  },
+                  child: Text("Google",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(height: 3.4, fontSize: 30)),
                 ),
-                SizedBox(width: 10),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      shadowColor: Colors.red[700],
-                      primary: Color.fromARGB(0, 255, 255, 255),
-                      padding: EdgeInsets.all(10),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20))),
-                  child: Container(
-                    height: 130,
-                    width: 150,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Color.fromARGB(255, 0, 0, 0),
-                      image: DecorationImage(
-                          colorFilter: ColorFilter.mode(
-                              Color.fromARGB(136, 0, 0, 0), BlendMode.darken),
-                          image: AssetImage('assets/netflix.jpg'),
-                          fit: BoxFit.cover),
-                    ),
-                    child: Text("Netflix",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(height: 3.4, fontSize: 30)),
+                // ),
+                onPressed: () {
+                  showGoogleDialog(context);
+                },
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    shadowColor: Colors.red[700],
+                    primary: Color.fromARGB(0, 255, 255, 255),
+                    padding: EdgeInsets.all(10),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20))),
+                child: Container(
+                  height: 130,
+                  width: 150,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    image: DecorationImage(
+                        colorFilter: ColorFilter.mode(
+                            Color.fromARGB(136, 0, 0, 0), BlendMode.darken),
+                        image: AssetImage('assets/netflix.jpg'),
+                        fit: BoxFit.cover),
                   ),
-                  // ),
-                  onPressed: () {
-                    showNetflixDialog(context);
-                  },
+                  child: Text("Netflix",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(height: 3.4, fontSize: 30)),
                 ),
-                SizedBox(width: 10),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      shadowColor: Colors.blue[300],
-                      primary: Color.fromARGB(0, 255, 255, 255),
-                      padding: EdgeInsets.all(10),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20))),
-                  child: Container(
-                    height: 130,
-                    width: 150,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Color.fromARGB(255, 0, 0, 0),
-                      image: DecorationImage(
-                          colorFilter: ColorFilter.mode(
-                              Color.fromARGB(136, 0, 0, 0), BlendMode.darken),
-                          image: AssetImage('assets/meta.jpg'),
-                          fit: BoxFit.cover),
-                    ),
-                    child: Text("Meta",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(height: 3.4, fontSize: 30)),
+                // ),
+                onPressed: () {
+                  showNetflixDialog(context);
+                },
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    shadowColor: Colors.blue[300],
+                    primary: Color.fromARGB(0, 255, 255, 255),
+                    padding: EdgeInsets.all(10),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20))),
+                child: Container(
+                  height: 130,
+                  width: 150,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    image: DecorationImage(
+                        colorFilter: ColorFilter.mode(
+                            Color.fromARGB(136, 0, 0, 0), BlendMode.darken),
+                        image: AssetImage('assets/meta.jpg'),
+                        fit: BoxFit.cover),
                   ),
-                  // ),
-                  onPressed: () {
-                    showMetaDialog(context);
-                  },
+                  child: Text("Meta",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(height: 3.4, fontSize: 30)),
                 ),
-                SizedBox(width: 10),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      primary: Color.fromARGB(0, 255, 255, 255),
-                      padding: EdgeInsets.all(10),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20))),
-                  child: Container(
-                    height: 130,
-                    width: 150,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Color.fromARGB(255, 0, 0, 0),
-                      image: DecorationImage(
-                          colorFilter: ColorFilter.mode(
-                              Color.fromARGB(136, 0, 0, 0), BlendMode.darken),
-                          image: AssetImage('assets/apple.jpg'),
-                          fit: BoxFit.cover),
-                    ),
-                    child: Text("Apple",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(height: 3.4, fontSize: 30)),
+                // ),
+                onPressed: () {
+                  showMetaDialog(context);
+                },
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    primary: Color.fromARGB(0, 255, 255, 255),
+                    padding: EdgeInsets.all(10),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20))),
+                child: Container(
+                  height: 130,
+                  width: 150,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    image: DecorationImage(
+                        colorFilter: ColorFilter.mode(
+                            Color.fromARGB(136, 0, 0, 0), BlendMode.darken),
+                        image: AssetImage('assets/apple.jpg'),
+                        fit: BoxFit.cover),
                   ),
-                  // ),
-                  onPressed: () {
-                    showAppleDialog(context);
-                  },
+                  child: Text("Apple",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(height: 3.4, fontSize: 30)),
                 ),
-                SizedBox(width: 10),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      shadowColor: Colors.green[800],
-                      primary: Color.fromARGB(0, 255, 255, 255),
-                      padding: EdgeInsets.all(10),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20))),
-                  child: Container(
-                    height: 130,
-                    width: 150,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Color.fromARGB(255, 0, 0, 0),
-                      image: DecorationImage(
-                          colorFilter: ColorFilter.mode(
-                              Color.fromARGB(136, 0, 0, 0), BlendMode.darken),
-                          image: AssetImage('assets/microsoft.jpg'),
-                          fit: BoxFit.cover),
-                    ),
-                    child: Text("Microsoft",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(height: 3.4, fontSize: 30)),
+                // ),
+                onPressed: () {
+                  showAppleDialog(context);
+                },
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    shadowColor: Colors.green[800],
+                    primary: Color.fromARGB(0, 255, 255, 255),
+                    padding: EdgeInsets.all(10),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20))),
+                child: Container(
+                  height: 130,
+                  width: 150,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    image: DecorationImage(
+                        colorFilter: ColorFilter.mode(
+                            Color.fromARGB(136, 0, 0, 0), BlendMode.darken),
+                        image: AssetImage('assets/microsoft.jpg'),
+                        fit: BoxFit.cover),
                   ),
-                  // ),
-                  onPressed: () {
-                    showMicrosoftDialog(context);
-                  },
+                  child: Text("Microsoft",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(height: 3.4, fontSize: 30)),
                 ),
-                SizedBox(width: 20),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      shadowColor: Colors.orange[800],
-                      primary: Color.fromARGB(0, 255, 255, 255),
-                      padding: EdgeInsets.all(10),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20))),
-                  child: Container(
-                    height: 130,
-                    width: 150,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Color.fromARGB(255, 0, 0, 0),
-                      image: DecorationImage(
-                          colorFilter: ColorFilter.mode(
-                              Color.fromARGB(136, 0, 0, 0), BlendMode.darken),
-                          image: AssetImage('assets/amazon.jpg'),
-                          fit: BoxFit.cover),
-                    ),
-                    child: Text("Amazon",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(height: 3.4, fontSize: 30)),
+                // ),
+                onPressed: () {
+                  showMicrosoftDialog(context);
+                },
+              ),
+              SizedBox(width: 20),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    shadowColor: Colors.orange[800],
+                    primary: Color.fromARGB(0, 255, 255, 255),
+                    padding: EdgeInsets.all(10),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20))),
+                child: Container(
+                  height: 130,
+                  width: 150,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    image: DecorationImage(
+                        colorFilter: ColorFilter.mode(
+                            Color.fromARGB(136, 0, 0, 0), BlendMode.darken),
+                        image: AssetImage('assets/amazon.jpg'),
+                        fit: BoxFit.cover),
                   ),
-                  // ),
-                  onPressed: () {
-                    showAmzonDialog(context);
-                  },
+                  child: Text("Amazon",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(height: 3.4, fontSize: 30)),
                 ),
-                SizedBox(width: 10),
-              ],
-            )));
+                // ),
+                onPressed: () {
+                  showAmzonDialog(context);
+                },
+              ),
+              SizedBox(width: 10),
+            ],
+          ),
+        ));
   }
 }
